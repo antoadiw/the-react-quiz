49db309e9d153b50687b3184fca032189b4dc59d
antoadiw <antoaw666@gmail.com>
2024-01-06 14:15:21 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,144 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Loader from "./components/Loader";
+import Error from "./components/Error";
+import StartScreen from "./components/StartScreen";
+import Question from "./components/Question";
+import { useEffect, useReducer } from "react";
+import Footer from "./components/Footer";
+import Progress from "./components/Progress";
+import NextButton from "./components/NextButton";
+import FinishScreen from "./components/FinishScreen";
+import Timer from "./components/Timer";
 
+const SEC_PER_QUESTION = 30;
+
+const initialState = {
+  questions: [],
+  answer: null,
+  points: 0,
+  index: 0,
+  highscore: 0,
+  secondRemaining: null,
+
+  // "Loading, Ready, Error, Active, Finished"
+  status: "Loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "Ready" };
+    case "dataFailed":
+      return { ...state, status: "Error" };
+    case "start":
+      return {
+        ...state,
+        secondRemaining: state.questions.length * SEC_PER_QUESTION,
+        status: "Active",
+      };
+    case "newAnswer":
+      const question = state.questions[state.index];
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
+    case "finish":
+      return {
+        ...state,
+        status: "Finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
+    case "restart":
+      return { ...initialState, questions: state.questions, status: "Ready" };
+    case "tick":
+      return {
+        ...state,
+        secondRemaining: state.secondRemaining - 1,
+        status: state.secondRemaining === 0 ? "finished" : state.status,
+      };
+    default:
+      throw new Error("unhandled action type");
+  }
+}
 function App() {
-  const [count, setCount] = useState(0)
+  const [
+    { questions, answer, points, index, highscore, secondRemaining, status },
+    dispatch,
+  ] = useReducer(reducer, initialState);
+  const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const res = await fetch("http://localhost:9000/questions");
+        const data = await res.json();
+        dispatch({ type: "dataReceived", payload: data });
+      } catch (err) {
+        dispatch({ type: "dataFailed" });
+      }
+    }
+    getData();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "Loading" && <Loader />}
+        {status === "Error" && <Error />}
+        {status === "Ready" && (
+          <StartScreen dispatch={dispatch} numQuestions={numQuestions} />
+        )}
+        {status === "Active" && (
+          <>
+            <Progress
+              maxPossiblePoints={maxPossiblePoints}
+              numQuestions={numQuestions}
+              index={index}
+              answer={answer}
+              points={points}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <Footer>
+              <NextButton
+                dispatch={dispatch}
+                index={index}
+                numQuestions={numQuestions}
+                answer={answer}
+              />
+              <Timer secondRemaining={secondRemaining} dispatch={dispatch} />
+            </Footer>
+          </>
+        )}
+        {status === "Finished" && (
+          <>
+            <FinishScreen
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              highscore={highscore}
+              dispatch={dispatch}
+            />
+          </>
+        )}
+      </Main>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="logo512.png" alt="React Logo" />
+      <h1>The React Quiz</h1>
+    </header>
+  );
+};
+
+export default Header;
